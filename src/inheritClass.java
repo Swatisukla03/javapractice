d3e459fe6cac962e5ceb9ef8c3c7d8f644b58808
Swati <swatisukla16@gmail.com>
2022-08-02 20:05:58 +0530

    'recentCommit'

---
+++ src/inheritClass.java
@@ -0,0 +1,23 @@
+class employee{
+    String name;
+    int emp_id;
+    int salary;
+    public void getInfo(){
+        System.out.println();
+    }
+
+}
+class programmer extends employee{
+
+}
+
+
+
+
+public class inheritClass {
+
+    public  static  void  main(String args[]){
+
+
+    }
+}
